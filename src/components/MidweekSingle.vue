b363223c2421a70a9eb06b835eea3624aa46805f
Chirag Parekh <chiragparekhn@gmail.com>
2017-04-16 12:35:18 +0530

    Accomplish follwoing tasks:
    1) Impliment the vue-sortable so that the elements can be sorted.
    2) Change all field types to be for loops (like hymns in original)
    3) Make it so that one side mirrors the other.

--- src/components/MidweekSingle.vue
+++ src/components/MidweekSingle.vue
@@ -8,10 +8,12 @@
           <h2>{{ item.fields.Date | moment }}</h2>
         </div><!--/header-->
         <div class="order">
-          <ol>
-            <li v-if="item.fields.Opening_Hymn"><strong>Hymn:</strong> {{ item.fields.Opening_Hymn ? item.fields.opening_hymn_number[0] + ', '+item.fields.opening_hymn_title[0] : '-' }}</li>
+          <draggable class="dragarea source" @start="drag=true" @end="onEnd">
+            <li v-if="item.fields.Opening_Hymn" v-for="(single_item,index) in item.fields.Opening_Hymn">
+              <strong>Hymn:</strong> {{ item.fields.opening_hymn_number[index] + ', '+item.fields.opening_hymn_title[index]}}
+            </li>
             <li><strong>Welcome &amp; Prayer</strong></li>
-            <li><strong>Missionary Letter: </strong>{{item.fields.Missionary_Name[0]}}</li>
+            <li v-if="item.fields.Missionary_Name" v-for="single_item in item.fields.Missionary_Name"><strong>Missionary Letter: </strong>{{ single_item }}</li>
             <li v-if="item.fields.Hymns" v-for="(hymn_item,index) in item.fields.Hymns">
               <strong>Hymn: </strong> {{ item.fields.Hymn_Number[index] }}, {{ item.fields.Hymn_Title[index] }}
             </li>
@@ -30,7 +32,7 @@
               <br>
               <em>{{item.fields.sermon_name[0]}}</em> <br>by {{item.fields.sermon_preacher[0]}}
             </li>
-          </ol>          
+          </draggable>      
         </div><!--/order-->
         </div><!--/half-page-->
         <div class="half-page">
@@ -39,7 +41,7 @@
         <h2>{{ item.fields.Date | moment }}</h2>
         </div><!--/header-->
         <div class="order">
-          <ol>
+          <div class="dragarea destination">
             <li v-if="item.fields.Opening_Hymn"><strong>Hymn:</strong> {{ item.fields.Opening_Hymn ? item.fields.opening_hymn_number[0] + ', '+item.fields.opening_hymn_title[0] : '-' }}</li>
             <li><strong>Welcome &amp; Prayer</strong></li>
             <li><strong>Missionary Letter: </strong>{{item.fields.Missionary_Name[0]}}</li>
@@ -61,7 +63,7 @@
               <br>
               <em>{{item.fields.sermon_name[0]}}</em> <br>by {{item.fields.sermon_preacher[0]}}
             </li>
-          </ol>          
+          </div>          
         </div><!--/order-->
         </div><!--/half-page-->
       </div><!--/page-->
@@ -71,7 +73,13 @@
 <script>
 import axios from 'axios'
 import moment from 'moment'
+import draggable from 'vuedraggable'
+import $ from "jquery";
+
 export default {
+  components: {
+      draggable,
+  },
   name: 'midweek_single',
   data:function(){
     return {
@@ -93,6 +101,9 @@ export default {
       }
     },
   methods:{
+    onEnd:function(event){
+      $(".dragarea.destination").html($(".dragarea.source").html());
+    },
     getAppCredentials:function(){
       if(localStorage.getItem('app_id'))
         this.app_id = localStorage.getItem('app_id');
@@ -120,5 +131,102 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-@charset "UTF-8";@import url(https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i);.page h1,.page h2{text-align:center;margin-top:0}.page h2,.page ol{font-style:italic}.page{width:11in;height:8.5in;padding:.25in;text-align:center;font-family:Lato,sans-serif;display:flex;justify-content:space-between}.half-page{width:4.75in}.page h1{font-size:20pt;weight:900;line-height:24pt;margin-bottom:0}.page h2{font-size:18pt;weight:400;line-height:100%}.page .header{border-bottom:1px solid #000;margin-bottom:.25in}.page ol{list-style-type:none;padding-left:0;margin:0;font-size:14pt;line-height:16pt;text-align:left}.page ol li{margin-bottom:.3in}.page ol li strong{font-style:normal;margin-right:.15in}.page ol li.offering{text-align:center}.page ol li.announcements,.page ol li.missionary,.page ol li.sermon{text-align:center;padding:.1in;background-color:#DADADA;border:1px solid #000}.page ol li.announcements strong,.page ol li.missionary strong,.page ol li.sermon strong{margin-right:0}.page ol li.announcements em,.page ol li.missionary em,.page ol li.sermon em{text-decoration:underline}.page ol li.announcements ul,.page ol li.missionary ul,.page ol li.sermon ul{list-style-type:none;margin-left:0;padding-left:0}.page ol li.announcements ul li:before,.page ol li.missionary ul li:before,.page ol li.sermon ul li:before{content:'•';margin-right:20px}.page ol li.announcements ul li ul,.page ol li.missionary ul li ul,.page ol li.sermon ul li ul{padding-left:31px;list-style:none}.page ol li.announcements ul li li:before,.page ol li.missionary ul li li:before,.page ol li.sermon ul li li:before{content:'—'}
+@charset "UTF-8";
+@import url(https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i);
+.page h1,
+.page h2 {
+    text-align: center;
+    margin-top: 0
+}
+.page h2,
+.page .dragarea {
+    font-style: italic
+}
+.page {
+    width: 11in;
+    height: 8.5in;
+    padding: .25in;
+    text-align: center;
+    font-family: Lato, sans-serif;
+    display: flex;
+    justify-content: space-between
+}
+.half-page {
+    width: 4.75in
+}
+.page h1 {
+    font-size: 20pt;
+    weight: 900;
+    line-height: 24pt;
+    margin-bottom: 0
+}
+.page h2 {
+    font-size: 18pt;
+    weight: 400;
+    line-height: 100%
+}
+.page .header {
+    border-bottom: 1px solid #000;
+    margin-bottom: .25in
+}
+.page .dragarea {
+    list-style-type: none;
+    padding-left: 0;
+    margin: 0;
+    font-size: 14pt;
+    line-height: 16pt;
+    text-align: left
+}
+.page .dragarea li {
+    margin-bottom: .3in
+}
+.page .dragarea li strong {
+    font-style: normal;
+    margin-right: .15in
+}
+.page .dragarea li.offering {
+    text-align: center
+}
+.page .dragarea li.announcements,
+.page .dragarea li.missionary,
+.page .dragarea li.sermon {
+    text-align: center;
+    padding: .1in;
+    background-color: #DADADA;
+    border: 1px solid #000
+}
+.page .dragarea li.announcements strong,
+.page .dragarea li.missionary strong,
+.page .dragarea li.sermon strong {
+    margin-right: 0
+}
+.page .dragarea li.announcements em,
+.page .dragarea li.missionary em,
+.page .dragarea li.sermon em {
+    text-decoration: underline
+}
+.page .dragarea li.announcements ul,
+.page .dragarea li.missionary ul,
+.page .dragarea li.sermon ul {
+    list-style-type: none;
+    margin-left: 0;
+    padding-left: 0
+}
+.page .dragarea li.announcements ul li:before,
+.page .dragarea li.missionary ul li:before,
+.page .dragarea li.sermon ul li:before {
+    content: '•';
+    margin-right: 20px
+}
+.page .dragarea li.announcements ul li ul,
+.page .dragarea li.missionary ul li ul,
+.page .dragarea li.sermon ul li ul {
+    padding-left: 31px;
+    list-style: none
+}
+.page .dragarea li.announcements ul li li:before,
+.page .dragarea li.missionary ul li li:before,
+.page .dragarea li.sermon ul li li:before {
+    content: '—'
+}
 </style>
--- src/components/SundayEveningSingle.vue
+++ src/components/SundayEveningSingle.vue
@@ -8,18 +8,18 @@
           <h2>{{ item.fields.Date | moment }}</h2>
         </div><!--/header-->
         <div class="order">
-          <ol>
-            <li v-if="item.fields.Opening_Hymn"><strong>Hymn:</strong> {{ item.fields.Opening_Hymn ? item.fields.opening_hymn_number[0] + ', '+item.fields.opening_hymn_title[0] : '-' }}</li>
-            <li><strong>Welcome &amp; Prayer</strong></li>
-            <span v-if="item.fields.Extra">
-            <li class="missionary" v-if="item.fields.extra_type[0] === 'Patch Singing'">{{ item.fields.extra_description[0] }}</li>
-            </span>
-            <li v-if="item.fields.chorus_name[0]"><strong>Chorus & Fellowship:</strong> {{item.fields.chorus_name[0]}}</li>
-            <span v-if='item.fields.Special'>
-            <li v-if="item.fields.special_position === 'Before Hymns'">
-              <strong>Special: </strong>{{ item.fields.Special_song_name[0] }} by {{ item.fields.Special_singer[0] }}
+          <draggable class="dragarea source" @start="drag=true" @end="onEnd">
+            <li v-if="item.fields.Opening_Hymn" v-for="(single_item,index) in item.fields.Opening_Hymn">
+              <strong>Hymn:</strong> {{ item.fields.opening_hymn_number[index] + ', '+item.fields.opening_hymn_title[index]}}
             </li>
-            </span>
+            <li><strong>Welcome &amp; Prayer</strong></li>
+            <li v-if="item.fields.Extra" class="missionary" v-for="(single_item,index) in item.fields.Extra">
+              {{ item.fields.extra_description[index] }}
+            </li>
+            <li v-if="item.fields.chorus_name" v-for="(single_item,index) in item.fields.chorus_name"><strong>Chorus & Fellowship:</strong> {{item.fields.chorus_name[index]}}</li>
+            <li v-if='item.fields.Special' v-for="(single_item,index) in item.fields.Special">
+              <strong>Special: </strong>{{ item.fields.Special_song_name[index] }} by {{ item.fields.Special_singer[index] }}
+            </li>
             <li v-if="item.fields.Hymns" v-for="(hymn_item,index) in item.fields.Hymns">
               <strong>Hymn: </strong> {{ item.fields.Hymn_Number[index] }}, {{ item.fields.Hymn_Title[index] }}
             </li>
@@ -32,24 +32,17 @@
                 </li>
               </ul>
             </li>
-            <span v-if="item.fields.Extra">
-            <li class="missionary" v-if="item.fields.extra_type[0] === 'Missionary'">{{ item.fields.extra_description[0] }}</li>
-            </span>
             <li class="offering"><strong>Offering</strong></li>
-            <span v-if='item.fields.Special'>
-            <li v-if="item.fields.special_position === 'Before Sermon'">
-              <strong>Special: </strong>{{ item.fields.Special_song_name[0] }} by {{ item.fields.Special_singer[0] }}
+            <li v-if='item.fields.Special' v-for="(single_item,index) in item.fields.Special">
+              <strong>Special: </strong>{{ item.fields.Special_song_name[index] }} by {{ item.fields.Special_singer[index] }}
             </li>
-            </span>
+            
             <li class="sermon" v-if="item.fields.Sermon">
               <strong>Sermon: </strong>
               <br>
               <em>{{item.fields.sermon_name[0]}}</em> <br>by {{item.fields.sermon_preacher[0]}}
             </li>
-            <span v-if="item.fields.Extra">
-            <li class="missionary" v-if="item.fields.extra_type[0] === 'Special Presentation'">{{ item.fields.extra_description[0] }}</li>
-            </span>
-          </ol>          
+          </draggable>         
         </div><!--/order-->
         </div><!--/half-page-->
         <div class="half-page">
@@ -58,18 +51,18 @@
           <h2>{{ item.fields.Date | moment }}</h2>
         </div><!--/header-->
         <div class="order">
-          <ol>
-            <li v-if="item.fields.Opening_Hymn"><strong>Hymn:</strong> {{ item.fields.Opening_Hymn ? item.fields.opening_hymn_number[0] + ', '+item.fields.opening_hymn_title[0] : '-' }}</li>
-            <li><strong>Welcome &amp; Prayer</strong></li>
-            <span v-if="item.fields.Extra">
-            <li class="missionary" v-if="item.fields.extra_type[0] === 'Patch Singing'">{{ item.fields.extra_description[0] }}</li>
-            </span>
-            <li v-if="item.fields.chorus_name[0]"><strong>Chorus & Fellowship:</strong> {{item.fields.chorus_name[0]}}</li>
-            <span v-if='item.fields.Special'>
-            <li v-if="item.fields.special_position === 'Before Hymns'">
-              <strong>Special: </strong>{{ item.fields.Special_song_name[0] }} by {{ item.fields.Special_singer[0] }}
+          <div class="dragarea destination">
+            <li v-if="item.fields.Opening_Hymn" v-for="(single_item,index) in item.fields.Opening_Hymn">
+              <strong>Hymn:</strong> {{ item.fields.opening_hymn_number[index] + ', '+item.fields.opening_hymn_title[index]}}
             </li>
-            </span>
+            <li><strong>Welcome &amp; Prayer</strong></li>
+            <li v-if="item.fields.Extra" class="missionary" v-for="(single_item,index) in item.fields.Extra">
+              {{ item.fields.extra_description[index] }}
+            </li>
+            <li v-if="item.fields.chorus_name" v-for="(single_item,index) in item.fields.chorus_name"><strong>Chorus & Fellowship:</strong> {{item.fields.chorus_name[index]}}</li>
+            <li v-if='item.fields.Special' v-for="(single_item,index) in item.fields.Special">
+              <strong>Special: </strong>{{ item.fields.Special_song_name[index] }} by {{ item.fields.Special_singer[index] }}
+            </li>
             <li v-if="item.fields.Hymns" v-for="(hymn_item,index) in item.fields.Hymns">
               <strong>Hymn: </strong> {{ item.fields.Hymn_Number[index] }}, {{ item.fields.Hymn_Title[index] }}
             </li>
@@ -82,24 +75,17 @@
                 </li>
               </ul>
             </li>
-            <span v-if="item.fields.Extra">
-            <li class="missionary" v-if="item.fields.extra_type[0] === 'Missionary'">{{ item.fields.extra_description[0] }}</li>
-            </span>
             <li class="offering"><strong>Offering</strong></li>
-            <span v-if='item.fields.Special'>
-            <li v-if="item.fields.special_position === 'Before Sermon'">
-              <strong>Special: </strong>{{ item.fields.Special_song_name[0] }} by {{ item.fields.Special_singer[0] }}
+            <li v-if='item.fields.Special' v-for="(single_item,index) in item.fields.Special">
+              <strong>Special: </strong>{{ item.fields.Special_song_name[index] }} by {{ item.fields.Special_singer[index] }}
             </li>
-            </span>
+            
             <li class="sermon" v-if="item.fields.Sermon">
               <strong>Sermon: </strong>
               <br>
               <em>{{item.fields.sermon_name[0]}}</em> <br>by {{item.fields.sermon_preacher[0]}}
             </li>
-            <span v-if="item.fields.Extra">
-            <li class="missionary" v-if="item.fields.extra_type[0] === 'Special Presentation'">{{ item.fields.extra_description[0] }}</li>
-            </span>
-          </ol>          
+          </div>          
         </div><!--/order-->
         </div><!--/half-page-->
       </div><!--/page-->
@@ -109,7 +95,13 @@
 <script>
 import axios from 'axios'
 import moment from 'moment'
+import draggable from 'vuedraggable'
+import $ from "jquery";
+
 export default {
+  components: {
+      draggable,
+  },
   name: 'sunday_evening_single',
   data:function(){
     return {
@@ -130,7 +122,10 @@ export default {
       return moment(date).format('dddd, MMMM Do, YYYY');
       }
     },
-  methods:{
+  methods:{    
+    onEnd:function(event){
+      $(".dragarea.destination").html($(".dragarea.source").html());
+    },
     getAppCredentials:function(){
       if(localStorage.getItem('app_id'))
         this.app_id = localStorage.getItem('app_id');
@@ -158,5 +153,102 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-@charset "UTF-8";@import url(https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i);.page h1,.page h2{text-align:center;margin-top:0}.page h2,.page ol{font-style:italic}.page{width:11in;height:8.5in;padding:.25in;text-align:center;font-family:Lato,sans-serif;display:flex;justify-content:space-between}.half-page{width:4.75in}.page h1{font-size:20pt;weight:900;line-height:24pt;margin-bottom:0}.page h2{font-size:18pt;weight:400;line-height:100%}.page .header{border-bottom:1px solid #000;margin-bottom:.25in}.page ol{list-style-type:none;padding-left:0;margin:0;font-size:14pt;line-height:16pt;text-align:left}.page ol li{margin-bottom:.3in}.page ol li strong{font-style:normal;margin-right:.15in}.page ol li.offering{text-align:center}.page ol li.announcements,.page ol li.missionary,.page ol li.sermon{text-align:center;padding:.1in;background-color:#DADADA;border:1px solid #000}.page ol li.announcements strong,.page ol li.missionary strong,.page ol li.sermon strong{margin-right:0}.page ol li.announcements em,.page ol li.missionary em,.page ol li.sermon em{text-decoration:underline}.page ol li.announcements ul,.page ol li.missionary ul,.page ol li.sermon ul{list-style-type:none;margin-left:0;padding-left:0}.page ol li.announcements ul li:before,.page ol li.missionary ul li:before,.page ol li.sermon ul li:before{content:'•';margin-right:20px}.page ol li.announcements ul li ul,.page ol li.missionary ul li ul,.page ol li.sermon ul li ul{padding-left:31px;list-style:none}.page ol li.announcements ul li li:before,.page ol li.missionary ul li li:before,.page ol li.sermon ul li li:before{content:'—'}
+@charset "UTF-8";
+@import url(https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i);
+.page h1,
+.page h2 {
+    text-align: center;
+    margin-top: 0
+}
+.page h2,
+.page .dragarea {
+    font-style: italic
+}
+.page {
+    width: 11in;
+    height: 8.5in;
+    padding: .25in;
+    text-align: center;
+    font-family: Lato, sans-serif;
+    display: flex;
+    justify-content: space-between
+}
+.half-page {
+    width: 4.75in
+}
+.page h1 {
+    font-size: 20pt;
+    weight: 900;
+    line-height: 24pt;
+    margin-bottom: 0
+}
+.page h2 {
+    font-size: 18pt;
+    weight: 400;
+    line-height: 100%
+}
+.page .header {
+    border-bottom: 1px solid #000;
+    margin-bottom: .25in
+}
+.page .dragarea {
+    list-style-type: none;
+    padding-left: 0;
+    margin: 0;
+    font-size: 14pt;
+    line-height: 16pt;
+    text-align: left
+}
+.page .dragarea li {
+    margin-bottom: .3in
+}
+.page .dragarea li strong {
+    font-style: normal;
+    margin-right: .15in
+}
+.page .dragarea li.offering {
+    text-align: center
+}
+.page .dragarea li.announcements,
+.page .dragarea li.missionary,
+.page .dragarea li.sermon {
+    text-align: center;
+    padding: .1in;
+    background-color: #DADADA;
+    border: 1px solid #000
+}
+.page .dragarea li.announcements strong,
+.page .dragarea li.missionary strong,
+.page .dragarea li.sermon strong {
+    margin-right: 0
+}
+.page .dragarea li.announcements em,
+.page .dragarea li.missionary em,
+.page .dragarea li.sermon em {
+    text-decoration: underline
+}
+.page .dragarea li.announcements ul,
+.page .dragarea li.missionary ul,
+.page .dragarea li.sermon ul {
+    list-style-type: none;
+    margin-left: 0;
+    padding-left: 0
+}
+.page .dragarea li.announcements ul li:before,
+.page .dragarea li.missionary ul li:before,
+.page .dragarea li.sermon ul li:before {
+    content: '•';
+    margin-right: 20px
+}
+.page .dragarea li.announcements ul li ul,
+.page .dragarea li.missionary ul li ul,
+.page .dragarea li.sermon ul li ul {
+    padding-left: 31px;
+    list-style: none
+}
+.page .dragarea li.announcements ul li li:before,
+.page .dragarea li.missionary ul li li:before,
+.page .dragarea li.sermon ul li li:before {
+    content: '—'
+}
 </style>
